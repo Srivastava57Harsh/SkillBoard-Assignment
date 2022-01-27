2cf38b1730958f2331b9c302af91d7cb6561f618
Srivastava57harsh <srivastava57harsh@gmail.com>
2022-01-27 19:58:18 +0530

    :sparkles: Add sidepanel
    Add sidepanel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,25 @@ function App() {
               <p className="para1">Available for job</p>
             </label>
           </div>
+
+          <label className="text2">
+            <img src="/home.svg" className="image2" />
+            <p className="para2">Home</p>
+          </label>
+          <div className="text1Container2">
+            <label className="content">
+              <img src="/assignment_turned_in.svg" className="image3" />
+              <p className="para3">Profile</p>
+            </label>
+            <label className="content2">
+              <img src="/stop.svg" className="image4" />
+              <p className="para4">Apply for Jobs</p>
+            </label>
+            <label className="content3">
+              <img src="/stop.svg" className="image5" />
+              <p className="para5">Mail Box</p>
+            </label>
+          </div>
         </div>
         <div className="cardTwo"></div>
         <div className="cardThree"></div>
