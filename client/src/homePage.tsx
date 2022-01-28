b5e6c3dc9de8132b72b4d0959272106fc1a9f8d8
Srivastava57harsh <srivastava57harsh@gmail.com>
2022-01-28 15:31:01 +0530

    :sparkles: add cards
    add cards

--- client/src/homePage.tsx
+++ client/src/homePage.tsx
@@ -8,7 +8,40 @@ export default function HomePage() {
         <Sidebar />
       </div>
       <div className="cardTwo">
-        <p className="Welcome">Welcome John!</p>
+        <p className="welcomeUsername">Welcome John!</p>
+        <br />
+        <p className="tagLine">How to get jobs with top U.S. companies </p>
+        <div className="cards">
+          <label className="label">
+            <p className="number">1</p>
+            <p>
+              Take 1<br />
+              Tests
+            </p>
+          </label>
+          <label className="label">
+            <p className="number">1</p>
+            <p>
+              Pass Live
+              <br />
+              Coding Challenges
+            </p>
+          </label>
+          <label className="label">
+            <p className="number">1</p>
+            <p>
+              Finalise <br />
+              your resume
+            </p>
+          </label>
+          <label className="label">
+            <p className="number">1</p>
+            <p>
+              Receive <br />
+              job offers
+            </p>
+          </label>
+        </div>
       </div>
     </div>
   );
