9b7c2d115bb36bd6cf783200a4eea85e4360f1cb
Srivastava57harsh <srivastava57harsh@gmail.com>
2022-01-28 17:38:50 +0530

    :construction: add cards on profile page
    add cards on profile page

--- client/src/profile.tsx
+++ client/src/profile.tsx
@@ -1,9 +1,28 @@
 import Sidebar from "./sidebar";
+import "./profile.css";
 
 export default function Profile() {
   return (
     <div className="gridContainer">
-      <Sidebar />
+      <div className="sideBar">
+        <Sidebar />
+      </div>
+      <div className="form">
+        <div className="uploadResume">
+          <label className="uploadLabel">
+            <img src="./resume.png" className="icons" />
+            <p className="text">
+              Upload your <br /> resume
+            </p>
+          </label>
+          <div className="uploadButtoncontainer">
+            <label className="uploadButton">
+              Let's start uploading your resume
+            </label>
+            <img src="./edit.png" className="icons2" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
