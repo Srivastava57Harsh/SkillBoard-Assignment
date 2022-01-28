b76435834c829f4b9e065513e13c609388279f6d
Srivastava57harsh <srivastava57harsh@gmail.com>
2022-01-28 16:05:43 +0530

    :sparkles: homepage ready
    homepage ready

--- client/src/homePage.tsx
+++ client/src/homePage.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
             </p>
           </label>
           <label className="label">
-            <p className="number">1</p>
+            <p className="number">2</p>
             <p>
               Pass Live
               <br />
@@ -28,20 +28,54 @@ export default function HomePage() {
             </p>
           </label>
           <label className="label">
-            <p className="number">1</p>
+            <p className="number">3</p>
             <p>
               Finalise <br />
               your resume
             </p>
           </label>
           <label className="label">
-            <p className="number">1</p>
+            <p className="number">4</p>
             <p>
               Receive <br />
               job offers
             </p>
           </label>
         </div>
+      </div>
+      <div className="cardThree">
+        <label className="canvas">
+          <img src="/vector.png" className="canvasImage" />
+          <div className="keyPoints">
+            <p>Why join</p>
+            <label className="points">
+              <p className="number">1</p>
+              <p className="textPoint">
+                Work full-time at top Silicon Valley or other U.S. companies.
+              </p>
+            </label>
+            <label className="points">
+              <p className="number">2</p>
+              <p className="textPoint">
+                Earn a better salary compared to local companies in your city.
+              </p>
+            </label>
+            <label className="points">
+              <p className="number">3</p>
+              <p className="textPoint">
+                Grow as a developer by working with the smartest engineers from
+                all over the world.
+              </p>
+            </label>
+            <label className="points">
+              <p className="number">4</p>
+              <p className="textPoint">
+                Get paid monthly directly to your bank account. Forget about
+                issues with PayPal or Payoneer.
+              </p>
+            </label>
+          </div>
+        </label>
       </div>
     </div>
   );
